b695227f937beeac4d6f4073e3f6a90abbd7fa77
sqr <kk.shichao@gmail.com>
2020-07-19 14:55:42 +0800

    refactor to def

--- read-log.py
+++ read-log.py
@@ -1,8 +1,7 @@
-# pylint: disable=E0401
 import re
 #import pandas as pd
 
-cov_log = r"D:\SNPS\Python_ex\cov-log-reader\idir\build-log.txt"
+cov_log_default = r"D:\SNPS\Python_ex\cov-log-reader\idir\build-log.txt"
 
 str_to_search = ["cov-build ", \
     "Dumping from hostname : ", \
@@ -13,20 +12,22 @@ str_to_search = ["cov-build ", \
     "Build time (cov-build overall):", \
     "compilation units"];
 
-search_index=0;
 
-with open(cov_log,'r') as file:
-    line = file.readline()
-    while line.strip():
-        
-        m = re.search(re.escape(str_to_search[search_index])+'(.+?)\n', line);
-        if m:
-            if(search_index<7):
-                print(m.group(1).strip())
-                search_index=search_index+1;
-            else:
-                print(line);
+def read_log_parse_1(cov_log):
+    search_index=0;
+    with open(cov_log,'r') as file:
         line = file.readline()
-        if(search_index==len(str_to_search)): break;
-        #print(line)
+        while line.strip():
+            
+            m = re.search(re.escape(str_to_search[search_index])+'(.+?)\n', line);
+            if m:
+                if(search_index<7):
+                    print(m.group(1).strip())
+                    search_index=search_index+1;
+                else:
+                    print(line);
+            line = file.readline()
+            if(search_index==len(str_to_search)): break;
+            #print(line)
 
+read_log_parse_1(cov_log_default);
