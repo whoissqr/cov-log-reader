a9cbaf6b7a630da46358ab42f760526ce0a0a972
siguser <chaoshi@synopsys.com>
2020-07-21 00:08:01 +0800

    fix a bug when log contains empty line; add pretty print

--- read-log.py
+++ read-log.py
@@ -1,12 +1,15 @@
 import re
+import os
 from collections import OrderedDict
+import pprint
 
-cov_log_default = r"D:\SNPS\Python_ex\cov-log-reader\idir\build-log.txt"
+cwd = os.getcwd()
+cov_log_default = cwd + r"/idir/build-log.txt"
 
 str_to_search_dict = OrderedDict([
     ("cov-build ", "none"),
     ("Dumping from hostname : ", "none"),
-    ("Platform info:", "none"),
+    ("Platform info", "none"),
     ("cov-build command:", "none"),
     ("Configuration read from: command", "none"),
     ("Configuration read from:", "none"),
@@ -20,18 +23,19 @@ def read_log_parse_1(cov_log):
     key_to_search = next(items)
     with open(cov_log, 'r') as file:
         line = file.readline()
-        while line.strip():
+        while line:
             m = re.search(re.escape(key_to_search)+'(.+?)\n', line)
             if m:
                 if(key_to_search != "compilation units"):
                     str_to_search_dict[key_to_search] = m.group(1).strip()
                     key_to_search = next(items)
                 else:
-                    print(line)
                     str_to_search_dict[key_to_search] += m.group(1).strip()
             line = file.readline()
     return str_to_search_dict
 
 
 read_log_parse_1(cov_log_default)
-print(str_to_search_dict)
+
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(str_to_search_dict)
